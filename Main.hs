7389505bd122e95879b05f85b51c1a37c3dc8da1
Hendrik128 <hendrikh@freenet.de>
2021-03-13 12:40:50 +0100

    Changing the dnn task in Main.hs for testing.

--- Main.hs
+++ Main.hs
@@ -17,10 +17,10 @@ import Text.Printf
 ------------------------------------------------------------------------------------
 -- important learning (hyper) parameters
 seed :: Int
-seed = 7
+seed = 128
 
 batchSize :: Int
-batchSize = 2
+batchSize = 1
 
 trainingSteps :: Int
 trainingSteps = 10000
@@ -29,33 +29,21 @@ learningRate :: Double
 learningRate = 0.0026
 
 lossFunction :: LossFunc
-lossFunction = crossEntropy
+lossFunction = squaredError
 
 ------------------------------------------------------------------------------------
 -- self created test data with binary labels
--- label is 1 iff 3rd input is greater than 0; 0 otherwise
+-- output should be addition of input
 
-features = initMatrix 10 3 [0.0, 0.0, 0.0,
-                            1.0, -3.0, 0.0,
-                            2.0, 4.0, 1.0,
-                            -3.0, 2.0, 4.0,
-                            2.24, 5.54, -7.6,
-                            3.4, -8.3, -2.0,
-                            -3.0, -9.0, 6.0,
-                            3.0, 2.0, 1.0,
-                            7.0, -9.0, -1.0,
-                            4.0, -8.0, 2.0]
+features = initMatrix 4 2 [0.0, 0.0,
+                            1.0, 0.0,
+                            0.0, 1.0,
+                            1.0, 1.0]
 
-labels = initMatrix 10 1 [0.0,
-                          0.0, 
+labels = initMatrix 4 1 [0.0,
                           1.0,
                           1.0,
-                          0.0,
-                          0.0,
-                          1.0,
-                          1.0,
-                          0.0,
-                          1.0]
+                          2.0]
 
 ------------------------------------------------------------------------------------
 
@@ -63,11 +51,11 @@ labels = initMatrix 10 1 [0.0,
 main :: IO ()
 main = do
     -- declare model as pipeline
-    let dnn = initDNN 3 [(DenseLayer, 10, relu), (DenseLayer, 2, relu)] kaiminWeightInit seed
-    let pipe = initPipeline Nothing dnn (Just softmax)
+    let dnn = initDNN 2 [(DenseLayer, 1, relu)] kaiminWeightInit seed
+    let pipe = initPipeline Nothing dnn Nothing
 
     -- one hot encoding labels
-    let oneHotEncodedLabels = oneHotEncoding labels 1
+    -- let oneHotEncodedLabels = oneHotEncoding labels 1
 
     -- get output of untrained network
     let pipeOutput1 = createPipeOutput pipe features
@@ -77,7 +65,7 @@ main = do
     printf "untrained accuracy: %.4f\n" untrainedAcc
 
     -- train the Neural network
-    let res = trainPipe seed trainingSteps batchSize learningRate lossFunction pipe features oneHotEncodedLabels
+    let res = trainPipe seed trainingSteps batchSize learningRate lossFunction pipe features labels
     let lossHistory = fst res
     let updatedPipe = snd res
 
